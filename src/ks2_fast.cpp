4003f52281676d833cdf7e1ef621408e04866f96
Miles Xi <nxi@ucla.edu>
2025-08-11 11:24:49 -0400

    Bump version

--- src/ks2_fast.cpp
+++ src/ks2_fast.cpp
@@ -1,25 +1,20 @@
-// [[Rcpp::depends(RcppArmadillo)]]
+// src/ks2_fast.cpp
 #include <Rcpp.h>
 using namespace Rcpp;
 
-//' Two-sample Kolmogorov–Smirnov D statistic (fast)
- //' @noRd
- // [[Rcpp::export]]
- double ks2_fast(NumericVector x, NumericVector y) {
-   int nx = x.size(), ny = y.size();
-   std::sort(x.begin(), x.end());
-   std::sort(y.begin(), y.end());
-   int i = 0, j = 0;
-   double cdf_x = 0.0, cdf_y = 0.0, dmax = 0.0;
+// [[Rcpp::export]]
+double ks2_fast(NumericVector x, NumericVector y) {
+  int nx = x.size(), ny = y.size();
+  std::sort(x.begin(), x.end());
+  std::sort(y.begin(), y.end());
+  int i = 0, j = 0;
+  double cdf_x = 0.0, cdf_y = 0.0, dmax = 0.0;
 
-   while (i < nx && j < ny) {
-     if (x[i] <= y[j]) {
-       ++i;   cdf_x = double(i) / nx;
-     } else {
-       ++j;   cdf_y = double(j) / ny;
-     }
-     double diff = std::fabs(cdf_x - cdf_y);
-     if (diff > dmax) dmax = diff;
-   }
-   return dmax;
- }
+  while (i < nx && j < ny) {
+    if (x[i] <= y[j]) { ++i; cdf_x = static_cast<double>(i)/nx; }
+    else              { ++j; cdf_y = static_cast<double>(j)/ny; }
+    double diff = std::fabs(cdf_x - cdf_y);
+    if (diff > dmax) dmax = diff;
+  }
+  return dmax;
+}
